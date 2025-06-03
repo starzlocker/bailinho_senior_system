40dc1a453b596f16c274e78aa1821c06fae29af1
Adolfo Gomes <adolfo.gomes@al.infnet.edu.br>
2025-06-02 21:25:09 -0300

    Declaração das funções no header do participantes

--- include/participantes.h
+++ include/participantes.h
@@ -1,1 +1,55 @@
-// Declarações das funções e structs de participantes
+#ifndef PARTICIPANTES_H
+#define PARTICIPANTES_H
+
+/**
+ * @struct Participante
+ * @brief Estrutura que armazena os dados de um participante
+ * 
+ */
+typedef struct {
+    char nome[50];
+    char cpf[12];
+    char celular[15];
+    int idade;
+} Participante;
+
+
+/**
+ * @brief Cadastra um novo participante no sistema
+ * 
+ * Solicita ao usuário informações sobre o novo participante e
+ * as armazena em um arquivo de texto (participantes.txt).
+ * 
+ * @return int 0 em caso de sucesso, 1 em caso de erro
+ */
+int participantes_create();
+
+/**
+ * @brief Lista todos os participantes cadastrados
+ * 
+ * @return int 0 em caso de sucesso, 1 em caso de erro
+ */
+int participantes_list();
+
+/**
+ * @brief Atualiza os dados de um participante específico
+ * 
+ * Solicita o CPF do participante a ser atualizado e, se encontrado,
+ * permite a edição de todos os seus dados. Utiliza um arquivo temporário
+ * para fazer a atualização.
+ * 
+ * @return int 0 em caso de sucesso, 1 em caso de erro ou participante não encontrado
+ */
+int participantes_update();
+
+/**
+ * @brief Apaga um participante específico pelo CPF
+ * 
+ * Solicita o CPF do participante a ser removido e o exclui do arquivo.
+ * Utiliza um arquivo temporário para a operação de exclusão.
+ * 
+ * @return int 0 em caso de sucesso, 1 em caso de erro
+ */
+int participantes_delete();
+
+#endif /* PARTICIPANTES_H */
