8f83eab2571f96c0a270f21b54db6d1bf7bd129d
jonny10 <araujolopes2776@gmail.com>
2025-06-11 20:17:42 -0300

    feat: finalizando validações

--- src/eventos.c
+++ src/eventos.c
@@ -4,6 +4,7 @@
 #include "../include/eventos.h"
 #include "../utils/utils.h"
 
+int valida; // Variável global para validação de entradas
 
 /**
  * @brief Cadastra um novo evento no sistema
@@ -16,7 +17,6 @@
 int eventos_create() {
     Evento evento;
     FILE *fp;
-    int status;
 
     fp = fopen("data/eventos.txt", "a+");
     if (fp == NULL) {
@@ -24,19 +24,70 @@ int eventos_create() {
         return 1;
     }
 
-    printf("Digite o nome do evento (Sem espaços): ");
-    scanf("%s", evento.nome);
-    printf("Digite a data do evento (No formato dd/mm/YY, ex: 30/05/2003): ");
-    scanf("%s", evento.data);
-    printf("Digite o horário do evento (No formato HH:MM 24horas, ex: 15:45): ");
-    scanf("%s", evento.horario);
-    printf("Digite o valor do evento: ");
-    scanf("%f", &evento.valorEntrada);
-    listar_status_evento();
-    printf("Digite o status do evento: ");
-    scanf("%d", &status);
+    do {
+        printf("Digite o nome do evento (Sem espaços): ");
+        scanf(" %49[^\n]", evento.nome);
 
-    strcpy(evento.status, status_evento(status));
+        valida = validar_string(evento.nome, 1, 49);
+
+        if (valida) {
+            printf("Nome inválido! Deve ter entre 1 e 50 caracteres.\n");
+            continue;
+        }
+
+        valida = contem_espaco(evento.nome);
+
+        if (valida) {
+            printf("Nome inválido! Não deve conter espaços.\n");
+        }
+    } while (valida == 1);
+
+    do {
+        printf("Digite a data do evento (No formato dd/mm/YY, ex: 30/05/2003): ");
+        scanf("%s", evento.data);
+
+        valida = validar_com_regex(evento.data, "^([0-2][0-9]|(3)[0-1])/(0[1-9]|1[0-2])/[0-9]{4}$");
+        if (valida) {
+            printf("Data inválida! Deve estar no formato dd/mm/YY.\n");
+        }
+    } while (valida == 1);
+
+    do {
+        printf("Digite o horário do evento (No formato HH:MM 24horas, ex: 15:45): ");
+        scanf("%s", evento.horario);
+
+        valida = validar_com_regex(evento.horario, "^([01][0-9]|2[0-3]):[0-5][0-9]$");
+        if (valida) {
+            printf("Horário inválido! Deve estar no formato HH:MM 24horas.\n");
+        }
+    } while (valida == 1);
+
+    do {
+        valida = 0;
+        printf("Digite o valor do evento: ");
+        scanf("%f", &evento.valorEntrada);
+
+        if (evento.valorEntrada < 0) {
+            printf("Valor inválido! Valor do evento deve ser um número positivo.\n");
+            valida = 1;
+        }
+    } while (valida == 1);
+
+    listar_status_evento();
+
+    do {
+        int status;
+
+        printf("Digite o status do evento: ");
+        scanf("%d", &status);
+        strcpy(evento.status, status_evento(status));
+
+        valida = strcmp(evento.status, "invalido") == 0;
+
+        if (valida) {
+            printf("Status inválido! Escolha um status válido.\n");
+        }
+    } while (valida);
 
     fprintf(
         fp,
@@ -97,7 +148,7 @@ int eventos_update() {
     FILE *fp_original, *fp_temp;
     Evento evento;
     char nome_alvo[50];
-    int encontrado = 0, status;
+    int encontrado = 0;
 
     printf("Digite o nome do evento que deseja alterar o status: ");
     scanf("%s", nome_alvo);
@@ -122,14 +173,27 @@ int eventos_update() {
 
         } else {
             encontrado = 1;
-            listar_status_evento();
-            printf("Digite o status do evento (atual: %s): ", evento.status);
-            scanf("%d", &status);
 
-            strcpy(evento.status, status_evento(status));
+            listar_status_evento();
+
+            do {
+                int status;
+
+                printf("Digite o status do evento (atual: %s): ", evento.status);
+                scanf("%d", &status);
+                strcpy(evento.status, status_evento(status));
+
+                valida = strcmp(evento.status, "invalido") == 0;
+
+                if (valida) {
+                    printf("Status inválido! Escolha um status válido.\n");
+                }
+            } while (valida);
+
             fprintf(fp_temp, "%s %s %s %f %s |\n", evento.nome, evento.data, evento.horario, evento.valorEntrada, evento.status);
         }
     }
+
     fclose(fp_original);
     fclose(fp_temp);
 
@@ -182,5 +246,7 @@ const char* status_evento(int status) {
             return "cancelado";
         case 3:
             return "agendado";
+        default:
+            return "invalido";
     }
 }
