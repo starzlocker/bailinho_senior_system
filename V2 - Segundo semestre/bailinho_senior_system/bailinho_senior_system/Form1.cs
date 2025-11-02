c934c877dd584e5dfd77a094f6df9ae03eda9bf8
adolfo <adolfo.gomes@al.infnet.edu.br>
2025-11-01 21:26:05 -0300

    feat: adição de botoes de plaholder no form1

--- V2 - Segundo semestre/bailinho_senior_system/bailinho_senior_system/Form1.cs
+++ V2 - Segundo semestre/bailinho_senior_system/bailinho_senior_system/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using bailinho_senior_system.views;
 
 namespace bailinho_senior_system
 {
@@ -15,6 +16,54 @@ namespace bailinho_senior_system
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void vendasBtn_Click(object sender, EventArgs e)
+        {
+            using (VendasView form = new VendasView())
+            {
+                form.ShowDialog();
+            }
+        }
+
+        private void produtosBtn_Click(object sender, EventArgs e)
+        {
+            using (ProdutosView form = new ProdutosView())
+            {
+                form.ShowDialog();
+            }
+        }
+
+        private void eventosBtn_Click(object sender, EventArgs e)
+        {
+            using (EventosView form = new EventosView())
+            {
+                form.ShowDialog();
+            }
+        }
+
+        private void fornecedoresBtn_Click(object sender, EventArgs e)
+        {
+            using (FornecedoresView form = new FornecedoresView())
+            {
+                form.ShowDialog();
+            }
+        }
+
+        private void clientesBtn_Click(object sender, EventArgs e)
+        {
+            using (ClientesView form = new ClientesView())
+            {
+                form.ShowDialog();
+            }
+        }
+
+        private void categoriasBtn_Click(object sender, EventArgs e)
+        {
+            using (CategoriasView form = new CategoriasView())
+            {
+                form.ShowDialog();
+            }
         }
     }
 }
