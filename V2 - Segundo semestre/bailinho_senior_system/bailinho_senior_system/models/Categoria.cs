736af9c84d19656b88b3d552c8244b83d12674f3
adolfo <adolfo.gomes@al.infnet.edu.br>
2025-11-02 20:58:36 -0300

    feat: criação dos models de produto e categoria

---
+++ V2 - Segundo semestre/bailinho_senior_system/bailinho_senior_system/models/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bailinho_senior_system.models
+{
+    internal class Categoria
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string nome;
+        public string Nome
+        { get { return nome; } set { nome = value; } }
+        private string descricao;
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+    }
+}
